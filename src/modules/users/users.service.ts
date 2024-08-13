3bf798465680211a16220c321c7e3b3b451c36b7
Moises Sousa <moises.sousa583@gmail.com>
2024-08-13 20:23:49 -0300

    [#] Integration of users module with Prisma

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,9 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+
+import { hash } from 'bcryptjs';
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return { createUserDto };
+  constructor(private readonly userRepository: UsersRepository) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const emailTaken = await this.userRepository.findUnique({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+
+    if (emailTaken) {
+      throw new ConflictException('This email is already in use');
+    }
+
+    const hashedPassword = await hash(createUserDto.password, 12);
+
+    return await this.userRepository.create({
+      data: {
+        name: createUserDto.name,
+        email: createUserDto.email,
+        password: hashedPassword,
+        categories: {
+          createMany: {
+            data: [
+              // INCOME
+              { name: 'Salário', icon: 'salary', type: 'INCOME' },
+              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+              // Expense
+              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
+              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
+              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
+              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
+              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
+              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
+              { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
+              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
+              { name: 'Outro', icon: 'other', type: 'EXPENSE' },
+            ],
+          },
+        },
+      },
+    });
   }
 }
